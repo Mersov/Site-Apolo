28163bf9515eaa33e33ad1e3f0b32d63858b8f9a
Mersov <emersonvsouza1800@gmail.com>
2025-04-18 13:14:19 -0300

    estruturas de site

--- src/pages/carros/hb20.tsx
+++
@@ -1,8 +0,0 @@
-export default function HB20() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl bg-red-500 text-cen">Welcome to the HB20 Page</h1>
-      <p className="mt-4 text-lg">This is a simple HB20 page.</p>
-    </div>
-  );
-}
