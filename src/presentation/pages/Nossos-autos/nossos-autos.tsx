aa498a3b84e015fba730443540e9f32168f7d3c3
Mersov <emersonvsouza1800@gmail.com>
2025-04-20 11:55:19 -0300

    Alteração de Caminhos

--- src/presentation/pages/Nossos-autos/nossos-autos.tsx
+++ src/presentation/pages/Nossos-autos/nossos-autos.tsx
@@ -1,4 +1,4 @@
-export default function NossosCarros() {
+export default function NossosAutos() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Nossos Carros</h1>
